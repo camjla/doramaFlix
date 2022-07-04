2939e14e3974381a194eee1bf220a231e4ca15e7
camjla <camila.tronco@hotmail.com>
2022-07-04 18:53:13 -0300

    Doramaflix

---
+++ js/owl/main.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:7,
+    nav:false,// tira a setinha
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:9
+        }
+    }
+
+})
